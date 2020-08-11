56877b58fbba0cf9f96890a334a909715237db66
mcnakhaee <mcnakhaee@gmail.org>
2020-08-11 12:30:29 +0200

    update package version

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
-
+import os
+import numpy
+import platform
 setup(name='palmerpenguins',
-      version='0.0.1',
+      version='0.1.1',
+      url="https://github.com/mcnakhaee/palmerpenguins",
       description="A python wrapper for palmer penguins's dataset",
+      long_description=open('README.md').read(),
       author='Muhammad Chenariyan Nakhaee',
       author_emai='mcnakhaee@gmail.com',
       packages = ['palmerpenguins'],
-      install_requires=['pandas', 'seaborn', 'numpy'])
+      install_requires=['pandas', 'seaborn', 'numpy'],
+      include_package_data=True,
+      package_data={'': ['data/*.csv']},
+      )
